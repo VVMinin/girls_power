90b962e6c60a9957a2fa568fed1fe6b5c8821780
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-05-30 21:24:26 +0300

    did timer, styles for timer and connect with modules

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -6,13 +6,49 @@ export class TimerModule extends Module {
   }
 
   trigger() {
+    const $containerElement = document.querySelector('.timer-module');
+    const $titleElement = document.querySelector('.timer-header');
     const $inputElement = document.querySelector('.timer-input');
     const $buttonElement = document.querySelector('.timer-button');
     const $timeLeftElement = document.querySelector('.time-left');
 
-    $buttonElement.addEventListener('click', () => {});
 
 
+    $buttonElement.addEventListener('click', (event) => {
+      // console.log('click');
+      const timeInput = parseInt($inputElement.value);
+      
+      if (!timeInput || timeInput <= 0) {
+        $inputElement.value = '';
+        // $inputElement.placeholder = 'Введите корректное число';
+        $timeLeftElement.textContent = 'Введите корректное число';
+        return 
+      } 
+      
+      
+      $titleElement.style.display = 'block';
+      $timeLeftElement.style.display = 'block';
+      $timeLeftElement.textContent = `Осталось: ${timeInput}`;
+      $inputElement.value = '';
+      
+      let timeSec = timeInput;
+
+      let timerId = setInterval(() => {
+            timeSec--;
+
+            if (timeSec >0) {
+              $timeLeftElement.textContent = `Осталось: ${timeSec} ` ;
+            } else {
+              clearInterval(timerId);
+              $timeLeftElement.textContent = 'Время вышло';
+              setTimeout(() => {
+                $titleElement.style.display = 'none';
+                $timeLeftElement.style.display = 'none';
+              }, 2000);
+        }
+      }, 1000)
+
+    });
   }
 
 
@@ -24,6 +60,7 @@ export class TimerModule extends Module {
     const $title = document.createElement('h2');
     $title.className = 'timer-header';
     $title.textContent = this.text;
+    $title.style.display = 'none';
 
     const $input = document.createElement('input');
     $input.className = 'timer-input';
@@ -36,10 +73,11 @@ export class TimerModule extends Module {
     const $timeLeft = document.createElement('p');
     $timeLeft.className = 'time-left'; 
     $timeLeft.textContent = `Осталось: `;
+    $timeLeft.style.display = 'none';
 
     $container.append($title, $input, $button, $timeLeft);
 
     return $container;
-  }
+  } 
 
 }
