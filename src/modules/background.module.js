dc6c5c50c5e3113d4fb8cdfef4c3b8cbe2688bde
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-05-30 20:20:01 +0300

    Добавила логику изменения цвета

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,9 @@
 import {Module} from '../core/module'
+import {getRandomColor} from '../utils';
 
 export class BackgroundModule extends Module {
-
+ trigger() {
+    const randomColor = getRandomColor()
+    parentElement.style.backgroundColor = randomColor
+  }
 }
