2e538d953a834ff660590234752ea80ced42ccf2
Vladimir Minin <kumayo23@gmail.com>
2025-05-30 19:06:19 +0300

    Меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
 import './styles.css'
+import { StaticMenu } from './menu'
 
+const menu = new StaticMenu('#menu', { x: 30, y: 30 })
+
+
+menu.add('Функция 1', () => alert('Выполнена Функция 1'))
+menu.add('Функция 2', () => alert('Выполнена Функция 2'))
+menu.add('Функция 3', () => alert('Выполнена Функция 3'))
+menu.add('Функция 4', () => alert('Выполнена Функция 4'))
+menu.add('Функция 5', () => alert('Выполнена Функция 5'))
+
+
+let isDragging = false
+let offsetX, offsetY
+
+menu.el.addEventListener('mousedown', (e) => {
+    isDragging = true
+    offsetX = e.clientX - menu.el.getBoundingClientRect().left
+    offsetY = e.clientY - menu.el.getBoundingClientRect().top
+    menu.el.style.cursor = 'grabbing'
+})
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return
+    menu.move(e.clientX - offsetX, e.clientY - offsetY)
+})
+
+document.addEventListener('mouseup', () => {
+    isDragging = false
+    menu.el.style.cursor = 'pointer'
+})
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,49 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
 
-export class ContextMenu extends Menu {
+export class StaticMenu extends Menu {
+    constructor(selector, position = { x: 20, y: 20 }) {
+        super(selector)
+        this.position = position
+        this.items = []
+        this.init()
+    }
 
+    init() {
+
+        this.el.style.display = 'block'
+        this.el.style.position = 'fixed'
+        this.el.style.left = `${this.position.x}px`
+        this.el.style.top = `${this.position.y}px`
+        this.el.style.zIndex = '10000'
+    }
+
+
+    open() {}
+    close() {}
+
+    add(text, callback) {
+        const item = { text, callback }
+        this.items.push(item)
+        this.render()
+    }
+
+    render() {
+        this.el.innerHTML = this.items.map(item => `
+      <li class="menu-item">
+        ${item.text}
+      </li>
+    `).join('')
+
+        this.el.querySelectorAll('.menu-item').forEach((item, index) => {
+            item.addEventListener('click', () => {
+                this.items[index].callback()
+            })
+        })
+    }
+
+
+    move(x, y) {
+        this.el.style.left = `${x}px`
+        this.el.style.top = `${y}px`
+    }
 }
