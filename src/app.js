a3d4cf127be91dfd937e63a313ccfeb8403bd4f6
Danilova Anna <annblacksea@gmail.com>
2025-05-31 22:28:12 +0300

    add message & cat modules

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-import './styles.css'
+import './styles.css';
+import './styles.css';
+import { CatModule } from './modules/cat.module';
 
+const cat = new CatModule('cat', 'Погладить кота');
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'q') {
+    cat.trigger();
+  }
+});
+console.log(cat);
