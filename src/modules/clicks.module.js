9ae85278c497e2f84660cc5e327d196bf2e7c3ac
Darina <darinakrasilnikova1@gmail.com>
2025-05-31 12:53:53 +0300

    Добавила модуль с аналитикой кликов

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,23 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
+import { timer, activationCounting, customAlert } from "../utils";
 
 export class ClicksModule extends Module {
+  trigger() {
+    let counters = { oneClick: 0, doubleClick: 0 };
+    let counting = true;
 
+    activationCounting(counters, counting);
+    timer();
+    new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(counters);
+        resolve(counters);
+      }, 5000);
+    }).then((counters) => {
+      counting = false;
+      customAlert(`Вы жмакнули: 
+        ${counters.oneClick - counters.doubleClick * 2} раз - одним кликом,
+        ${counters.doubleClick} раз - двойным кликом.`);
+    });
+  }
 }
