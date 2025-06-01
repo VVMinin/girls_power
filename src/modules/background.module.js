bb5098c9820e0e2e85877cd0447be3976603028b
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-06-01 09:39:29 +0300

    исправленна ошибка в случайном фоне

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -4,6 +4,6 @@ import {getRandomColor} from '../utils'
 export class BackgroundModule extends Module {
  trigger() {
     const randomColor = getRandomColor()
-    parentElement.style.backgroundColor = randomColor
+     document.body.style.backgroundColor = randomColor
   }
 }
